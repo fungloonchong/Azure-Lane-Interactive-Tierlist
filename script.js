4afea6304635d04b5d7bdaebebc1b8ee0136c851
Bandaras213 <Bandaras213@gmail.com>
2020-08-11 15:48:09 +0200

    🤮

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let maincont;
+let ships;
+
+window.onload = async function(){
+  maincont = document.getElementsByClassName("main")[0]
+  ships = await getjson()
+  buildhtml()
+}
+
 async function getjson() {
     try {
     let data = await fetch("ships.json")
@@ -7,6 +16,68 @@ async function getjson() {
       console.error(e);
     }
   }
+
+async function buildhtml() {
+  let t = document.createElement("div");
+  t.className = "destroyer";
+  maincont.appendChild(t);
+  let s = document.createElement("div");
+  s.className = "t0";
+  document.getElementsByClassName("destroyer")[0].appendChild(s);
+  await filltier(t.className, s.className);
   
-  getjson()
-  .then(result => console.log(result.destroyer.t0[0].tags.en))
+}
+
+async function filltier(a1, a2) {
+  console.log(ships[`${a1}`][`${a2}`])
+ for (let i = 0; i < ships[`${a1}`][`${a2}`].length; i++) {
+ // Main div
+ let a = document.createElement("div");
+  a.className = "parent";
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].appendChild(a);
+  // thumbnail
+  a = document.createElement("img");
+  a.className = "thumbnail";
+  a.src = ships[`${a1}`][`${a2}`][i].thumbnail;
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].appendChild(a);
+  // Bannerright
+  if (ships[`${a1}`][`${a2}`][i].banneralt != null) {
+  a = document.createElement("img");
+  a.className = "bannerright";
+  a.src = ships[`${a1}`][`${a2}`][i].bannerlink;
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].appendChild(a);
+  }
+  // Bannerleft
+  if (ships[`${a1}`][`${a2}`][i].banneralt != null) {
+  a = document.createElement("img");
+  a.className = "bannerleft";
+  a.src = ships[`${a1}`][`${a2}`][i].banneraltlink;
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].appendChild(a);
+  }
+  // Tags
+  for (let ii = 0; ii < ships[`${a1}`][`${a2}`][i].tags.length; ii++) {
+  a = document.createElement("img");
+  a.className = "tag" + (ii + 1);
+  a.src = "Assets/TagIcons/EN/" + ships[`${a1}`][`${a2}`][i].tags[ii] + ".png";
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].appendChild(a);
+  }
+  // Greyblock
+  a = document.createElement("img");
+  a.className = "greyblock";
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].appendChild(a);
+  // Hulltype
+  a = document.createElement("img");
+  a.className = "hulltype";
+  a.src = "Assets/HullTypeIcons/" + ships[`${a1}`][`${a2}`][i].hullTypeId + ".png";
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].appendChild(a);
+  // Textblock
+  a = document.createElement("div");
+  a.className = "text";
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].appendChild(a);
+  // Span text
+  a = document.createElement("span");
+  a.className = "shipname";
+  a.innerHTML = ships[`${a1}`][`${a2}`][i].names.en
+  document.getElementsByClassName(a1)[0].getElementsByClassName(a2)[0].getElementsByClassName("parent")[i].getElementsByClassName("text")[0].appendChild(a);
+}
+}
